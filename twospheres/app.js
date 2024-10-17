ea0250f5c6c73b0c3481a000a17f75befb13408e
Avike Seal <avikeseal@gmail.com>
2024-10-17 11:41:44 -0400

    added lighting

--- twospheres/app.js
+++ twospheres/app.js
@@ -5,3 +5,11 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer, domElement);
+
+// Adding lighting for 3D effect
+const ambientLight = new THREE.AmbientLight(0x404040); // Soft ambient light
+scene.add(ambientLight);
+
+const pointLight = new THREE.PointLight(0xffffff, 1, 1000) // Strong point light
+pointLight.position.set(5, 5, 5); //position light to shine on the spheres
+scene.add(pointLight);
